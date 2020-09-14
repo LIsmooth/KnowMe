ea48aeadddee321b0da21384246c7ac1cae5dd06
Leif <helloleif@foxmail.com>
2020-09-14 22:32:03 +0800

    Logback config

--- src/main/java/com/leif/knowme/controller/WxController.java
+++ src/main/java/com/leif/knowme/controller/WxController.java
@@ -5,6 +5,8 @@ import com.leif.knowme.pojo.WxUserInfo;
 import com.leif.knowme.service.AccountService;
 import com.leif.knowme.service.WxService;
 import com.leif.knowme.util.CryptUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +24,7 @@ public class WxController {
     AccountService accountService;
     @Autowired
     WxService wxService;
+    Logger logger = LoggerFactory.getLogger(getClass());
 
     @PostMapping(value = "/register", headers = "Accept=application/json")
     public void wxRegister(@RequestBody Map<String,String> params) throws AppException {
@@ -35,5 +38,9 @@ public class WxController {
 
     @GetMapping(value="/login/{code}")
     public void wxLogin(@PathVariable String code){
+        logger.trace("trace log");
+        logger.debug("debug log");
+        logger.warn("warn log");
+        logger.error("error",new AppException("this is an Exception"));
     }
 }
